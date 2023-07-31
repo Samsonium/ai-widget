346891596b3b25e9d3e3bc8b80a5437d401bb876
Samsonium <tdesu@vk.com>
2023-07-31 11:45:13 +0300

    Not little changes

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -1,149 +1,169 @@
 import {
-  app,
-  BrowserWindow,
-  Notification,
-  // nativeImage
-} from "electron";
-import { join } from "path";
-import { parse } from "url";
-import { autoUpdater } from "electron-updater";
+    app,
+    BrowserWindow,
+    Notification,
+    ipcMain
+    // nativeImage
+} from 'electron';
+import {join} from 'path';
+import {parse} from 'url';
+import {autoUpdater} from 'electron-updater';
 
-import logger from "./utils/logger";
-import settings from "./utils/settings";
+import logger from './utils/logger';
+import settings from './utils/settings';
 
-const isProd = process.env.NODE_ENV === "production" || app.isPackaged;
+const isProd = process.env.NODE_ENV === 'production' || app.isPackaged;
 
-logger.info("App starting...");
-settings.set("check", true);
-logger.info("Checking if settings store works correctly.");
-logger.info(settings.get("check") ? "Settings store works correctly." : "Settings store has a problem.");
+logger.info('App starting...');
+settings.set('check', true);
+logger.info('Checking if settings store works correctly.');
+logger.info(settings.get('check') ? 'Settings store works correctly.' : 'Settings store has a problem.');
 
 let mainWindow: BrowserWindow | null;
 let notification: Notification | null;
 
 const createWindow = () => {
-  mainWindow = new BrowserWindow({
-    width: 900,
-    height: 680,
-    webPreferences: {
-      devTools: isProd ? false : true,
-      contextIsolation: true,
-    },
-  });
+    const {width, height} = require('electron').screen.getPrimaryDisplay().workAreaSize
 
-  const url =
-    // process.env.NODE_ENV === "production"
-    isProd
-      ? // in production, use the statically build version of our application
-        `file://${join(__dirname, "public", "index.html")}`
-      : // in dev, target the host and port of the local rollup web server
-        "http://localhost:5000";
+    mainWindow = new BrowserWindow({
+        width: 500,
+        height: height - 100,
+        transparent: true,
+        titleBarStyle: 'hidden',
+        alwaysOnTop: true,
+        movable: false,
+        resizable: false,
+        webPreferences: {
+            devTools: !isProd,
+            preload: join(__dirname, 'preload.js'),
+            contextIsolation: true,
+            webSecurity: false
+        },
+    });
 
-  mainWindow.loadURL(url).catch((err) => {
-    logger.error(JSON.stringify(err));
-    app.quit();
-  });
+    const url =
+        // process.env.NODE_ENV === "production"
+        isProd
+            ? // in production, use the statically build version of our application
+            `file://${join(__dirname, 'public', 'index.html')}`
+            : // in dev, target the host and port of the local rollup web server
+            'http://localhost:5000';
 
-  if (!isProd) mainWindow.webContents.openDevTools();
+    mainWindow.loadURL(url).then(() => {
+        mainWindow?.setPosition(width - 500, 50)
+    }).catch((err) => {
+        logger.error(JSON.stringify(err));
+        app.quit();
+    });
 
-  mainWindow.on("closed", () => {
-    mainWindow = null;
-  });
+    if (!isProd) mainWindow.webContents.openDevTools({
+        mode: 'detach',
+    });
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+
+    ipcMain.handle('win:focus', (_, data) => {
+        mainWindow?.setIgnoreMouseEvents(!data, {
+            forward: true
+        })
+    })
 };
 
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
 // those two events are completely optional to subscrbe to, but that's a common way to get the
 // user experience people expect to have on macOS: do not quit the application directly
 // after the user close the last window, instead wait for Command + Q (or equivalent).
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
 });
 
-app.on("activate", () => {
-  if (mainWindow === null) createWindow();
+app.on('activate', () => {
+    if (mainWindow === null) createWindow();
 });
 
-app.on("web-contents-created", (e, contents) => {
-  logger.info(e);
-  // Security of webviews
-  contents.on("will-attach-webview", (event, webPreferences, params) => {
-    logger.info(event, params);
-    // Strip away preload scripts if unused or verify their location is legitimate
-    delete webPreferences.preload;
+app.on('web-contents-created', (e, contents) => {
+    logger.info(e);
+    // Security of webviews
+    contents.on('will-attach-webview', (event, webPreferences, params) => {
+        logger.info(event, params);
+        // Strip away preload scripts if unused or verify their location is legitimate
+        delete webPreferences.preload;
 
-    // Disable Node.js integration
-    webPreferences.nodeIntegration = false;
+        // Disable Node.js integration
+        webPreferences.nodeIntegration = false;
 
-    // Verify URL being loaded
-    // if (!params.src.startsWith(`file://${join(__dirname)}`)) {
-    //   event.preventDefault(); // We do not open anything now
-    // }
-  });
+        // Verify URL being loaded
+        // if (!params.src.startsWith(`file://${join(__dirname)}`)) {
+        //   event.preventDefault(); // We do not open anything now
+        // }
+    });
 
-  contents.on("will-navigate", (event, navigationUrl) => {
-    const parsedURL = parse(navigationUrl);
-    // In dev mode allow Hot Module Replacement
-    if (parsedURL.host !== "localhost:5000" && !isProd) {
-      logger.warn("Stopped attempt to open: " + navigationUrl);
-      event.preventDefault();
-    } else if (isProd) {
-      logger.warn("Stopped attempt to open: " + navigationUrl);
-      event.preventDefault();
-    }
-  });
+    contents.on('will-navigate', (event, navigationUrl) => {
+        const parsedURL = parse(navigationUrl);
+        // In dev mode allow Hot Module Replacement
+        if (parsedURL.host !== 'localhost:5000' && !isProd) {
+            logger.warn('Stopped attempt to open: ' + navigationUrl);
+            event.preventDefault();
+        } else if (isProd) {
+            logger.warn('Stopped attempt to open: ' + navigationUrl);
+            event.preventDefault();
+        }
+    });
 });
 
 if (isProd)
-  autoUpdater.checkForUpdates().catch((err) => {
-    logger.error(JSON.stringify(err));
-  });
+    autoUpdater.checkForUpdates().catch((err) => {
+        logger.error(JSON.stringify(err));
+    });
 
 autoUpdater.logger = logger;
 
-autoUpdater.on("update-available", () => {
-  notification = new Notification({
-    title: "Electron-Svelte-Typescript",
-    body: "Updates are available. Click to download.",
-    silent: true,
-    // icon: nativeImage.createFromPath(join(__dirname, "..", "assets", "icon.png"),
-  });
-  notification.show();
-  notification.on("click", () => {
-    autoUpdater.downloadUpdate().catch((err) => {
-      logger.error(JSON.stringify(err));
+autoUpdater.on('update-available', () => {
+    notification = new Notification({
+        title: 'Electron-Svelte-Typescript',
+        body: 'Updates are available. Click to download.',
+        silent: true,
+        // icon: nativeImage.createFromPath(join(__dirname, "..", "assets", "icon.png"),
     });
-  });
+    notification.show();
+    notification.on('click', () => {
+        autoUpdater.downloadUpdate().catch((err) => {
+            logger.error(JSON.stringify(err));
+        });
+    });
 });
 
-autoUpdater.on("update-not-available", () => {
-  notification = new Notification({
-    title: "Electron-Svelte-Typescript",
-    body: "Your software is up to date.",
-    silent: true,
-    // icon: nativeImage.createFromPath(join(__dirname, "..", "assets", "icon.png"),
-  });
-  notification.show();
+autoUpdater.on('update-not-available', () => {
+    notification = new Notification({
+        title: 'Electron-Svelte-Typescript',
+        body: 'Your software is up to date.',
+        silent: true,
+        // icon: nativeImage.createFromPath(join(__dirname, "..", "assets", "icon.png"),
+    });
+    notification.show();
 });
 
-autoUpdater.on("update-downloaded", () => {
-  notification = new Notification({
-    title: "Electron-Svelte-Typescript",
-    body: "The updates are ready. Click to quit and install.",
-    silent: true,
-    // icon: nativeImage.createFromPath(join(__dirname, "..", "assets", "icon.png"),
-  });
-  notification.show();
-  notification.on("click", () => {
-    autoUpdater.quitAndInstall();
-  });
+autoUpdater.on('update-downloaded', () => {
+    notification = new Notification({
+        title: 'Electron-Svelte-Typescript',
+        body: 'The updates are ready. Click to quit and install.',
+        silent: true,
+        // icon: nativeImage.createFromPath(join(__dirname, "..", "assets", "icon.png"),
+    });
+    notification.show();
+    notification.on('click', () => {
+        autoUpdater.quitAndInstall();
+    });
 });
 
-autoUpdater.on("error", (err) => {
-  notification = new Notification({
-    title: "Electron-Svelte-Typescript",
-    body: JSON.stringify(err),
-    // icon: nativeImage.createFromPath(join(__dirname, "..", "assets", "icon.png"),
-  });
-  notification.show();
+autoUpdater.on('error', (err) => {
+    notification = new Notification({
+        title: 'Whooooops!',
+        body: JSON.stringify(err),
+        // icon: nativeImage.createFromPath(join(__dirname, "..", "assets", "icon.png"),
+    });
+    notification.show();
 });
--- src/frontend/App.svelte
+++ src/frontend/App.svelte
@@ -1,30 +1,168 @@
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
-
 <script lang="ts">
-  export let name: string;
+  import Chat from './Chat.svelte';
+
+  let expanded = true
+  $: window.core?.focus(expanded)
+
+  function handleMouseOver(type: boolean) {
+    if (expanded) return;
+    window.core?.focus(type)
+  }
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="holder">
+  <div class="box" class:collapsed={!expanded}>
+    <button class="expander"
+            on:click={() => expanded = !expanded}
+            on:mouseover={handleMouseOver.bind(null, true)}
+            on:mouseout={handleMouseOver.bind(null, false)}>
+      {#if expanded}
+        &searr;
+      {:else}
+        &nwarr;
+      {/if}
+    </button>
+
+    <div class="ai-icon">
+      AI
+    </div>
+
+    <div class="content">
+      <div class="header">
+        <h3>AI ассистент</h3>
+      </div>
+      <Chat />
+    </div>
+  </div>
+</div>
+
+<style>
+  .holder {
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: flex-end;
+    justify-content: flex-end;
+    background: transparent;
+    pointer-events: none;
+    user-select: none;
+  }
+
+  .box {
+    position: relative;
+    width: calc(100% - 8px);
+    height: calc(100% - 8px);
+    background: #2d2d2d;
+    pointer-events: visiblePainted;
+    display: flex;
+    flex-flow: row;
+    align-items: center;
+    margin-bottom: 1px;
+    justify-content: center;
+    border-top-left-radius: 16px;
+    border-bottom-left-radius: 16px;
+    outline: 1px solid #414141;
+    transition: width .5s cubic-bezier(.25, 0, 0, 1),
+                height .5s cubic-bezier(.25, 0, 0, 1),
+                background .5s cubic-bezier(.25, 0, 0, 1);
+  }
+
+  .box.collapsed {
+    width: 50px;
+    height: 50px;
+    background: #efefef;
+  }
+
+  button.expander {
+    position: absolute;
+    z-index: 100;
+    left: -4px;
+    top: -4px;
+    height: 32px;
+    width: 32px;
+    border-radius: 16px;
+    background: #262e2f;
+    color: white;
+    outline: 1px solid #414141;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    border: none;
+    box-shadow: 0 4px 16px rgba(0, 0, 0, .15);
+    transition: background .2s cubic-bezier(.25, 0, 0, 1),
+                outline .2s cubic-bezier(.25, 0, 0, 1),
+                box-shadow .2s cubic-bezier(.25, 0, 0, 1),
+                opacity .2s cubic-bezier(.25, 0, 0, 1);
+  }
+
+  button.expander:hover {
+    background: #22292a;
+    outline-width: 3px;
+    opacity: 1 !important;
+  }
+
+  .content {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-flow: column;
+    align-items: stretch;
+    justify-content: flex-start;
+    overflow: hidden;
+    border-top-left-radius: 16px;
+    border-bottom-left-radius: 16px;
+    transition: opacity .15s cubic-bezier(.25, 0, 0, 1);
+  }
+
+  .box.collapsed .content {
+    opacity: 0;
+  }
+
+  .box.collapsed button.expander {
+    box-shadow: 0 2px 4px rgba(0, 0, 0, .15);
+    opacity: .75;
+  }
+
+  .ai-icon {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font: 700 32px Inter, monospace;
+    color: #22292a;
+    pointer-events: none;
+    padding: 0;
+    margin: 0;
+    align-self: center;
+    opacity: 0;
+    transition: opacity .5s cubic-bezier(.25, 0, 0, 1);
+  }
+
+  .box.collapsed .ai-icon {
+    opacity: 1;
+  }
+
+  .header {
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: center;
+    background: #2d3236;
+    border-bottom: 1px solid #414141;
+  }
+
+  .header h3 {
+    font: 500 16px Inter, sans-serif;
+    color: white;
+  }
+</style>
